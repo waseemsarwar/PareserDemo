fb59d60dd2104c261f509802ae2bde9e01db4c5b
hummy <hummy@waseems-macbook.(none)>
2012-05-13 11:26:46 +0500

    Initial Commit

---
+++ PareserDemo/PareserDemoAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  PareserDemoAppDelegate.h
+//  PareserDemo
+//
+//  Created by hummy on 5/13/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class PareserDemoViewController;
+
+@interface PareserDemoAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet PareserDemoViewController *viewController;
+
+@end
---
+++ PareserDemo/PareserDemoViewController.h
@@ -0,0 +1,15 @@
+//
+//  PareserDemoViewController.h
+//  PareserDemo
+//
+//  Created by hummy on 5/13/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PareserDemoViewController : UIViewController {
+    
+}
+
+@end
